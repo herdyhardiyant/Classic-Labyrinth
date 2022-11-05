4ea82dc3a1cc8031708fbefb2dbc21912c9bf1a6
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2022-11-05 12:36:49 +0700

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // TODO enable gyro or accelerometer
+    // TODO create labyrinth with walls
+    // TODO Camera sees the labyrinth
+    // TODO create key to collect
+    // TODO Create UI to show the key collected
+    // TODO Make the ball move with the gyroscope gravity
+    // TODO after the player collect all the keys, the game is over
+    // TODO create a menu to start the game
+    // TODO Zoom in and out the camera with two fingers
+    // TODO Move camera with fingers
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
