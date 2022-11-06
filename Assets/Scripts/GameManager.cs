24f248fd8721643be5851453a74a03ba553bb796
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2022-11-06 10:08:46 +0700

    Update map

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // TODO enable gyro or accelerometer
-    // TODO create labyrinth with walls
-    // TODO Camera sees the labyrinth
-    // TODO create key to collect
-    // TODO Create UI to show the key collected
-    // TODO Make the ball move with the gyroscope gravity
-    // TODO after the player collect all the keys, the game is over
-    // TODO create a menu to start the game
-    // TODO Zoom in and out the camera with two fingers
-    // TODO Move camera with fingers
 
     [SerializeField] private UIManager uiManager;
 
